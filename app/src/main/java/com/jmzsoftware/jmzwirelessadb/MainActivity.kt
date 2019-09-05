9b72c74c1f962bd284188d9787f67fbb784ca205
Harsh Shandilya <msfjarvis@gmail.com>
2019-09-05 18:41:02 +0530

    Update for Android 10
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ app/src/main/java/com/jmzsoftware/jmzwirelessadb/MainActivity.kt
@@ -0,0 +1,75 @@
+/**
+ * Jmz Wireless ADB
+ *
+ * Copyright 2016 by Jmz Software <support></support>@jmzsoftware.com>
+ *
+ *
+ * Some open source application is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Some open source application is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code.  If not, see https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
+package com.jmzsoftware.jmzwirelessadb
+
+import android.content.Context
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import eu.chainfire.libsuperuser.Shell
+
+class MainActivity : AppCompatActivity() {
+    private val ip: String
+        get() {
+            val mWifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val ip = mWifiManager.connectionInfo.ipAddress
+            return ((ip and 0xFF).toString() + "." + (ip shr 8 and 0xFF) + "." + (ip shr 16 and 0xFF) + "."
+                    + (ip shr 24 and 0xFF))
+
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
+        val adb = findViewById<Button>(R.id.button)
+        adb.setOnClickListener {
+            if (adb.text == resources.getString(R.string.disable)) {
+                disableAdb()
+                textView.text = ""
+                adb.text = resources.getString(R.string.enable)
+            } else {
+                enableAdb()
+                textView.text = resources.getString(R.string.noti, ip)
+                adb.text = resources.getString(R.string.disable)
+            }
+        }
+    }
+
+    private fun enableAdb() {
+        runCommands(arrayOf("setprop service.adb.tcp.port 5555", "stop adbd", "start adbd"))
+    }
+
+    private fun disableAdb() {
+        runCommands(arrayOf("stop adbd"))
+    }
+
+    private fun runCommands(cmds: Array<String>) {
+        try {
+            Shell.Pool.SU.run(cmds)
+        } catch (ignored: Shell.ShellDiedException) {
+        }
+
+    }
+}
